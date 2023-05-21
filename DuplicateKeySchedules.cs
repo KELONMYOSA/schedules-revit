36ce020df542bc2a75d0c003576caf4fcd6e6ba3
Vladislav Samoilenko <79825223592@yandex.ru>
2023-05-21 18:37:53 +0300

    now opens revit files from path

--- DuplicateKeySchedules.cs
+++ DuplicateKeySchedules.cs
@@ -1,6 +1,13 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+using Document = Autodesk.Revit.DB.Document;
 
 namespace Schedules
 {
@@ -9,9 +16,17 @@ namespace Schedules
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            Application app = commandData.Application.Application;
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
-            var ui = new UserInterfaceDuplicateKeySchedules();
+            IEnumerable<ViewSchedule> keySchedules = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Schedules)
+                .Cast<ViewSchedule>()
+                .Where(v => v.Definition.IsKeySchedule == true);
+
+            IList<string> keyScheduleNames = keySchedules.Select(v => v.Name).ToList();
+
+            var ui = new UserInterfaceDuplicateKeySchedules(keyScheduleNames);
             bool tdRes = (bool)ui.ShowDialog();
 
             if (tdRes == false)
@@ -20,8 +35,39 @@ namespace Schedules
             }
             else
             {
+                string selectedFolder = ui.selectedFolder;
+                IList<string> selectedSchedulesNames = ui.selectedSchedules;
+
+                IList<ViewSchedule> selectedSchedules = keySchedules.Where(v => selectedSchedulesNames.Contains(v.Name)).ToList();
+                IList<string> revitFilesPaths = Directory.EnumerateFiles(selectedFolder, "*.rvt", SearchOption.AllDirectories)
+                    .Where(f => !f.Equals(doc.PathName)).ToList();
+
+                foreach (string path in revitFilesPaths)
+                {
+                    Document openedDoc = OpenDocumentWithoutWorksets(app, ModelPathUtils.ConvertUserVisiblePathToModelPath(path));
+                    
+                }
+                
                 return Result.Succeeded;
             } 
+        }
+
+        private Document OpenDocumentWithoutWorksets(Application app, ModelPath projectPath)
+        {
+            Document doc = null;
+            try
+            {
+                OpenOptions openOptions = new OpenOptions();
+                WorksetConfiguration openConfig = new WorksetConfiguration(WorksetConfigurationOption.CloseAllWorksets);
+                openOptions.SetOpenWorksetsConfiguration(openConfig);
+                doc = app.OpenDocumentFile(projectPath, openOptions);
+            }
+            catch (Exception e)
+            {
+                TaskDialog.Show("Open File Failed", e.Message);
+            }
+
+            return doc;
         }
     }
 }
--- UserInterfaceDuplicateKeySchedules.xaml.cs
+++ UserInterfaceDuplicateKeySchedules.xaml.cs
@@ -1,18 +1,43 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Schedules
 {
     public partial class UserInterfaceDuplicateKeySchedules : Window
     {
-        public UserInterfaceDuplicateKeySchedules()
+        public UserInterfaceDuplicateKeySchedules(IList<string> schedulesList)
         {
             InitializeComponent();
+            SchedulesBox.ItemsSource = schedulesList;            
+        }
+
+        public IList<string> selectedSchedules
+        {
+            get
+            {
+                return SchedulesBox.SelectedItems.Cast<string>().ToList();
+            }
+        }
+
+        public string selectedFolder
+        {
+            get
+            {
+                return FolderPath.Text;
+            }
         }
 
         private void ButtonDuplicate(Object sender, EventArgs e)
         {
+            if (SchedulesBox.SelectedItems.Count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("Спецификации не выбраны!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             if (FolderPath.Text == "Выберите папку")
             {
                 System.Windows.Forms.MessageBox.Show("Папка не выбрана!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
